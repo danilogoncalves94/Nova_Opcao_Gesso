3cf29a571fd28f93ad11d47763903c05d4566b24
danilogoncalves94 <danilogoncalvesdg21@gmail.com>
2022-12-11 15:52:22 -0300

    upgrades

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function mytitle() {
     titulo.style.cssText = 'color: white;'
 
     setTimeout(function () {
-        topoSite.style.cssText = "background-image: url('/imagens/imagem_fundo.jpg');"
+        topoSite.style.cssText = "background-image: url('imagens/imagem_fundo.jpg');"
     }, 1000)
 
     setTimeout(function () {
         topoSite.style.cssText =
-            "background-image: url('/imagens/imagem_fundo.jpg');" +
+            "background-image: url('imagens/imagem_fundo.jpg');" +
             "margin-bottom: 100px";
 
         content.style.cssText = "display: contents;"
